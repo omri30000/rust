faeab81883494e306cbe27eca1db1b7ec8e52a57
Omri Zaiman <omri162004@gmail.com>
2022-10-03 12:39:20 +0300

    added solution stable+clean version

--- exercises/practice/space-age/src/lib.rs
+++ exercises/practice/space-age/src/lib.rs
@@ -1,38 +1,87 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+const SECONDS_IN_YEAR: f64 = 31557600.0;
+
+enum PlanetScale {
+    Mercury,
+    Venus,
+    Earth,
+    Mars,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune,
+}
+impl PlanetScale {
+    fn value(&self) -> f64 {
+        match *self {
+            PlanetScale::Mercury => 0.2408467,
+            PlanetScale::Venus => 0.61519726,
+            PlanetScale::Earth => 1.0,
+            PlanetScale::Mars => 1.8808158,
+            PlanetScale::Jupiter => 11.862615,
+            PlanetScale::Saturn => 29.447498,
+            PlanetScale::Uranus => 84.016846,
+            PlanetScale::Neptune => 164.79132,
+        }
+    }
+}
 
 #[derive(Debug)]
-pub struct Duration;
-
+pub struct Duration(f64);
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration(s as f64 / SECONDS_IN_YEAR)
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
 pub struct Mercury;
+impl Planet for Mercury {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Mercury.value()
+    }
+}
 pub struct Venus;
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Venus.value()
+    }
+}
 pub struct Earth;
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Earth.value()
+    }
+}
 pub struct Mars;
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Mars.value()
+    }
+}
 pub struct Jupiter;
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Jupiter.value()
+    }
+}
 pub struct Saturn;
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Saturn.value()
+    }
+}
 pub struct Uranus;
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Uranus.value()
+    }
+}
 pub struct Neptune;
-
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 / PlanetScale::Neptune.value()
+    }
+}
